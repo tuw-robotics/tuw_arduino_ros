318d811e62c9e7fffca7a5f78afc1f8ba5c51eeb
Marc-Ubuntu <marc.dhaubenstock@gmail.com>
2018-06-05 10:29:50 +0200

    using correction rpm to linear vel equation

--- tuw_rccar/src/tuw_rccar_node.cpp
+++ tuw_rccar/src/tuw_rccar_node.cpp
@@ -143,7 +143,7 @@ void RCCarNode::publish () {
     measurement_iws_.header.stamp = ros::Time::now();
     // robot cant turn on the spot
     measurement_iws_.steering[0] = std::abs(actuator_rps) > ACTUATOR_RPS_EPS ? actuators_.rad*100.0 : 0.0;
-    measurement_iws_.revolute[1] = wheel_radius*actuator_rps*-1.0f; // direction seems inverse to rev
+    measurement_iws_.revolute[1] = (wheel_diameter_*M_PI*actuator_rps*-1.0f)/60.0; // direction seems inverse to rev
 
     float achsabstand = 0.26;
 
